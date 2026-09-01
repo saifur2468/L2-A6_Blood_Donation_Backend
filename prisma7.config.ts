6c1a405d0ee2d96383ea08fd8bebaafcd9627e40
Your GitHub Username <Your GitHub Email>
2026-09-01 23:20:01 +0600

    feat:Add database schema

--- prisma7.config.ts
+++ prisma7.config.ts
@@ -4,11 +4,11 @@ import "dotenv/config";
 import { defineConfig } from "prisma/config";
 
 export default defineConfig({
-  schema: "prisma/schema.prisma",
+  schema: "prisma/schema/schema.prisma",
   migrations: {
     path: "prisma/migrations",
   },
-  datasource: {
-    url: process.env["DATABASE_URL"],
+  datasource:{
+    url:process.env["DATABASE_URL"],
   },
 });
